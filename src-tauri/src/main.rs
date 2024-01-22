2cbff6363352d2c7c51dc833a94f3f9aef1f3202
Michael Pfister <pfister@contria.com>
2024-01-22 17:22:21 +0100

    use tauri_plugin_sql

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,7 @@
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler!(convert_markdown))
+        .plugin(tauri_plugin_sql::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import Editor from "./Editor.jsx";
+import {useState} from "react";
 
 function App() {
 
-  return (
-    <Editor />
-  );
+    const [notes, setNotes] = useState([1]);
+    const noteItems = notes.map((item) =>
+        <div className="flex flex-row justify-between items-center bg-green-200 border-4 border-green-700">
+            <div className="bg-green-50 cursor-pointer w-full h-full"><h2>Note</h2></div>
+            <button className="btn btn-sm">Delete me</button>
+        </div>
+    );
+
+    return (
+        <div className="bg-gray-700 h-screen p-2">
+            <div className="flex flex-row justify-between items-center">
+                <h1 className="text-white font-bold">All notes</h1>
+                <button className="btn btn-sm" onClick={() => setNotes([...notes, 1])}>Add notes</button>
+            </div>
+            <input className="my-2 w-full input"></input>
+            {noteItems}
+        </div>
+    );
 }
 
 export default App;
